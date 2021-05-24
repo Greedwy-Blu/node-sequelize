a0cbbb4354acca03a97f9d68bba173670485bb9b
Gabriel Oliveira <greedwyblu@gmail.com>
2021-05-24 00:44:11 -0300

    node

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 require('./database');
+
+
+
 const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.use(express.static('public'));
+
+
 
 app.use(express.json());
 
